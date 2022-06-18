42ef3911f12ef04a2802f0d2bed82b08557da273
cendanaawal <89263129+cendanaawal@users.noreply.github.com>
2022-06-18 14:33:07 +0000

    1

--- main.py
+++ main.py
@@ -1,1 +1,14 @@
-tes
+import streamlit as st
+
+st.write("""
+# Aplikasi Luas Segi Tiga
+Ini aplikasi menggunakan streamli """)
+
+a = st.number_input("Masukkan luas alas a: ", 0)
+t = st.number_input("Masukkan masukkan tinggi : ", 0)
+hitung = st.button("Hitung")
+
+if hitung:
+    luas = a*t/2
+    st.success(f"Luas segitiga adalah :  {luas}")
+    st.balloons()
